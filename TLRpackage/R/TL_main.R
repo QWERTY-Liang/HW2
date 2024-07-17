532ee639bb5f28aa74c8f9dd0ce8641af4310a73
QWERTY-Liang <tongliang0409@outlook.com>
2024-07-17 12:37:34 +0100

    basic ready

---
+++ TLRpackage/R/TL_main.R
@@ -0,0 +1,74 @@
+#' Read in HDI Data, export a new class with correct title
+#'
+#' @param file  file (hdro_indicators_COUNTRYNAME.csv)
+#'
+#' @return new class "HDI".
+#' @export
+#'
+#' @examples
+#' function(file)
+TL_read <- function(file) {
+  data <- fread(file,skip=1)#read.csv(file, stringsAsFactors = TRUE)
+  setnames(data, c("country_code", "country_name", "indicator_id", "indicator_name", "index_id", "index_name", "value", "year"))
+  class(data) <- c("HDI", class(data))
+  return(data)
+}
+
+
+
+
+#' Print Method for HDI Class
+#'
+#' @param x new HDI Class
+#' @param ... Further arguments
+#'
+#' @return None. Prints the first 10 rows
+#' @export
+#'
+#' @examples
+#' data <- TL_read("path/to/hdro_indicators_aggregates_irl.csv")
+#' TL_print(data)
+TL_print <- function(x, ...) {
+
+  print(head(x, 2))
+}
+
+#' Summary Method for HDI Class
+#'
+#' @param x new HDI Class
+#' @param ... Further arguments
+#'
+#' @return summary of the new HDI object.
+#' @export
+#'
+#' @examples
+#' data <- TL_read("path/to/hdro_indicators_aggregates_irl.csv")
+#' TL_summary(data)
+TL_summary <- function(x, ...) {
+  summary(x)
+}
+
+
+
+#' Plot Method for HDI Class
+#'
+#' @param x new HDI Class
+#' @param ... Further arguments
+#'
+#' @return a plot
+#' @export
+#'
+#' @examples
+#' data <- TL_read("path/to/hdro_indicators_aggregates_irl.csv")
+#' TL_plot(data)
+TL_plot <- function(x, ...) {
+  ggplot(x$'indicator_id', aes(x=country_name, y=value)) +
+    geom_boxplot(fill="slateblue", alpha=0.2) +
+    xlab("Country name")+
+    ggtitle("Life Expectancy at Birth (years)")
+}
+
+#setMethod("print", "HDI", TL_print)
+#setMethod("summary", "HDI", TL_summary)
+#setMethod("plot", "HDI", TL_plot)
+
---
+++ TLRpackage/README.md
@@ -0,0 +1,25 @@
+# TLRpackage
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of TLRpackage is to read and summarise the Human Development Indicators data.
+
+## Installation
+
+You can install the development version of TLRpackage from [GitHub](https://github.com/) with:
+https://github.com/QWERTY-Liang/HW2.git
+``` r
+# install.packages("devtools")
+devtools::install_github("QWERTY-Liang/HW2")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(TLRpackage)
+## basic example code
+```
